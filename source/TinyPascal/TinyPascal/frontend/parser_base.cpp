0babdab01ee8b23d02c842d0f16ba257bbab8812
Blair Davidson <blair.joel.davidson@gmail.com>
2012-05-24 15:15:09 +0800

    token type fast dispatch

--- source/TinyPascal/TinyPascal/frontend/parser_base.cpp
+++ source/TinyPascal/TinyPascal/frontend/parser_base.cpp
@@ -7,29 +7,19 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-#ifndef _PARSER_BASE_H_
-#define _PARSER_BASE_H_
-
-#include <string>
-#include <boost/noncopyable.hpp>
-#include <memory>
-#include "token_type.h"
-#include "token.h"
-
-#ifdef _WIN32
-#include "../intermediate/abstract_syntax_tree_node.h"
-#else
-#include "abstract_syntax_tree_node.h"
-#endif
+#include "parser_base.h"
 
 namespace pascal{
 	namespace frontend{
-		class parser_base : public boost::noncopyable{
-		public:
-			parser_base();
-			virtual ~parser_base() = 0;
-			
-		};
+		parser_base::parser_base()
+		{
+		}
+		
+		parser_base::~parser_base()
+		{
+		}
+
+
+
 	}
 }
-#endif
